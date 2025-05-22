eb04172f48fad427a5cde1e51f27a0fabc64e1b6
Indra Purnomo <indra.purnomo@mail.ugm.ac.id>
2025-05-22 17:41:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>UTS-Indra-Purnomo-PemogramanWebII</title>
+    <!-- Indra Purnomo, 230401020031, IF401, Pemograman Web II -->
     <!-- Bootstrap pake CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
